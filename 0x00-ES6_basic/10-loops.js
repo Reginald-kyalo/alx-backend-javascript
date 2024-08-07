fbd51e91c981542908fac2931bbc2be94a877aa2
Reginald-kyalo <reginald.kyalo@gmail.com>
2024-08-07 05:19:01 +0300

    files to be eslint compliant

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  const newArray = [...array];
+  
+  for (const [idx, value] of array.entries()) {
+    newArray[idx] = appendString + value;
+  }
+  
+  return newArray;
 }
