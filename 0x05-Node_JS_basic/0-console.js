44b32f16f27bef4d0499d3e1387468043cb213ee
Reginald-kyalo <reginald.kyalo@gmail.com>
2024-10-03 23:44:58 +0300

    update

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,10 @@
-module.exports = function displayMessage(string) {
-    process.stdout.write(string);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Reginald Kyalo <https://github.com/Reginald-kyalo>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
