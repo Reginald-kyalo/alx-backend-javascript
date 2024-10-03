b40046b31ae2569fe77b829edc2e7e8e18d756e4
Reginald-kyalo <reginald.kyalo@gmail.com>
2024-10-03 23:37:49 +0300

    nodejs basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,3 +2,14 @@
 welcomeMessage = 'Welcome to Holberton School, what is your name?'
 console.log(welcomeMessage)
 
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,17 @@
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+/**
+ * Binds the routes to the appropriate handler in the
+ * given Express application.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Reginald Kyalo <https://github.com/Reginald-kyalo>
+ */
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
